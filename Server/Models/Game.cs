f0620b7c4ae8318818d248f688a864eec9052917
Zunzap <chlo1259@gmail.com>
2015-01-09 13:29:36 +0100

    First Commit Server

---
+++ Server/Models/Game.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Server.Models
+{
+    public class Game
+    {
+
+        public int Id { get; set; }
+        public int Score1 { get; set; }
+        public int Score2 { get; set; }
+    }
+}
